81a441cf8bc69b44986e1967d92d74747090a4a6
linyang4 <lin.yang4@outlook.com>
2019-10-16 05:22:08 +0000

    Deploying to gh-pages from master ff3c2867c67af7c8eab65bbeb1b7e81d541562c1

---
+++ precache-manifest.8d68678e550b054baca0c5bc4485708f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a30ea84dc8d82b59ef7d9f70169eee1",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "90bbda5b6401cf19f3ae",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8e5e684506bc4cd6bd35",
+    "url": "/github-actions-demo/static/js/2.33ddc6e3.chunk.js"
+  },
+  {
+    "revision": "90bbda5b6401cf19f3ae",
+    "url": "/github-actions-demo/static/js/main.ca7254a1.chunk.js"
+  },
+  {
+    "revision": "5afa295eceed7b45ef4d",
+    "url": "/github-actions-demo/static/js/runtime-main.7cc9f103.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
